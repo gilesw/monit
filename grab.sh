d98374fc7cebaca98d9e6c0279c5b6930f252606
Giles Westwood <giles.westwood@gmail.com>
2023-10-03 13:20:40 +0100

    stash a new version

--- grab.sh
+++ grab.sh
@@ -3,8 +3,8 @@
 wget -q https://mmonit.com/monit/  -O /tmp/monit.html
 MONIT_VERSION=$(grep 'dist/binary' /tmp/monit.html | head -1 | perl -ne '/binary\/([\d|\.]*)\/monit/ && print $1')
 current_dir=`pwd`
-#if [[ ! -d $MONIT_VERSION ]];then
-#  mkdir $MONIT_VERSION
+if [[ ! -d $MONIT_VERSION ]];then
+  mkdir $MONIT_VERSION
   cd $MONIT_VERSION
   for bin in `grep 'dist/binary/' /tmp/monit.html | perl -ne '/(monit-.*)\" class/ && print "$1 "'`;do
     monit_url=https://mmonit.com/monit/dist/binary/$MONIT_VERSION/$bin
@@ -12,7 +12,7 @@ current_dir=`pwd`
     wget $monit_url
     sleep 5
   done
-#fi
+fi
 cd $current_dir
 
 
